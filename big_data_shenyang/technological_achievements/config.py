34dd3716d05e0a81819c8fec035526127fe4e5f1
littlesluttery <53028837+littlesluttery@users.noreply.github.com>
2021-05-15 15:48:21 +0800

    Update config.py

--- big_data_shenyang/technological_achievements/config.py
+++ big_data_shenyang/technological_achievements/config.py
@@ -3,7 +3,7 @@ CONFIG = {
         'host': '127.0.0.1',
         'port': 3306,
         'user': 'root',
-        'password': '671203',
+        'password': '密码',
         'db': 'big_data_shenyang',
         'charset': 'utf8',
     }
